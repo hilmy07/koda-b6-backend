5386bae108a50de4f4a21ac9e522adfdecc568af
mrrcodex <mrramadhandev@gmail.com>
2026-03-11 09:57:40 +0700

    feat: add migration forgot password

---
+++ internal/repository/forgot.go
@@ -0,0 +1,19 @@
+package repository
+
+import "github.com/jackc/pgx/v5"
+
+type ForgotPasswordRepository struct {
+	db *pgx.Conn
+}
+
+func NewForgotPasswordRepository(db *pgx.Conn) *ForgotPasswordRepository {
+	return &ForgotPasswordRepository{db: db}
+}
+
+func (r *ForgotPasswordRepository) GetDataByEmailnCode() {
+
+}
+
+func (r *ForgotPasswordRepository) DeleteDataByCode() {
+	
+}
--- migrations/000001_init_db.up.sql
+++ migrations/000001_init_db.up.sql
@@ -66,3 +66,4 @@ CREATE TABLE IF NOT EXISTS product_reviews (
 );
 
 
+
