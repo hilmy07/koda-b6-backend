2d658ece767f47bebd824efe1ae5de57506a33c4
mrrcodex <mrramadhandev@gmail.com>
2026-04-06 09:26:12 +0700

    fix: update endpoint get carts by user

--- internal/models/cart.go
+++ internal/models/cart.go
@@ -19,7 +19,7 @@ type CartByUserID struct {
 	Size         string    `json:"size" db:"size"`
 	Variant      string    `json:"variant" db:"variant"`
 	UserId       int       `json:"user_id" db:"user_id"`
-	ProductName  string    `json:"product_id" db:"name_product"`
+	ProductName  string    `json:"name_product" db:"name_product"`
 	Price        int       `json:"price" db:"base_price"`
 	Created_at   time.Time
 	Updated_at   time.Time
